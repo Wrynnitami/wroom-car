a3808547fc916b7689b6cda6309ec3eb03cb14bd
Wrynnitami <52575832+Wrynnitami@users.noreply.github.com>
2024-05-19 19:26:43 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -63,6 +63,9 @@ class Car:
     def get_сolor(self):
         return self.color
 
+    def set_color(self, color):
+        self.color = color
+
     def fuel(self):
 
         print(f"current fuel is {self.current_fuel}")
@@ -70,17 +73,26 @@ class Car:
     def get_fuel(self):
         return self.current_fuel
 
+    def set_fuel(self, fuel):
+        self.current_fuel = fuel
+
     def gas_tank(self):
         pass
 
     def get_gas_tank(self):
         return self.gas_tank
 
+    def set_gas_tank(self, gas_tank):
+        self.gas_tank = gas_tank
+
     def consumption(self):
         print(f"current consumption of fuel is {self.consumption}")
 
     def get_consumption(self):
         return self.consumption
+
+    def set_consumption(self, cons):
+        self.consumption = cons
 
     def __repr__(self):
         return repr(self.make)
@@ -97,6 +109,10 @@ class Car:
         self.consumption = consumption
 
 
+# Attributes = dict.fromkeys(["make", "model", "color", "gas tank", "current fuel", "consumption"]) /тут я напортачил
+# с сеттерами и поэтому думал как решить этому проблему, думал прибегнуть к словарю, для присваивания значений
+# ,но не пригодилось
+
 try:
     while True:
         to_cars = int(input("Create new vehicle or pick up one from the list \n""for create vehicle, type 1\n""for "
@@ -109,24 +125,43 @@ try:
 
             if create_car == 0:
                 Cars.append(Car(makeAndModel))
+                continue
 
             if create_car == 1:
-                print("you can add a several attributes, like a : color of car, volume of tank, current amount of "
+                Cars.append(Car(makeAndModel))
+
+                print("you can add a several attributes \nlike a : color of car, volume of tank, current amount of "
                       "fuel and fuel consumption")
-                custom_car = int(input("for indicate a color of the car press - 1 \n""for indicate a volume of tank "
-                                       "press - 2 for indicate a current amount of fuel press - 3 \n""for indicate a "
-                                       "fuel consumption press - 4 "))
 
-                if custom_car == 1:
+            custom_car = int(input("for indicate the color of the car press - 1 \n""for indicate the volume of tank"
+                                   " press - 2\n""for indicate a current amount of fuel press - 3 \n""for indicate the "
+                                   "consumption of fuel press - 4 \n"":"))
 
-                if custom_car == 2:
+            if custom_car == 1:
+                car_color = (input("insert color for created car : "))
+                Cars[-1].set_color(car_color)
+                    # custom_continue = input("if you want to continue, please press 1\nif you want to stop press 2:\n")
 
-                if custom_car == 3:
+                    # if custom_continue == 1:
+                    #     continue
+                    #
+                    # if custom_continue == 2:
+                    #     break
 
-                if custom_car == 4:
+            if custom_car == 2:
+                gt = (input("insert the volume of tank for created car : "))
+                Cars[-1].set_gas_tank(gt)
 
+            if custom_car == 3:
+                cur_fuel = (input("insert the current amount of fuel for created car : "))
+                Cars[-1].set_fuel(cur_fuel)
 
+            if custom_car == 4:
+                cons = (input("insert the consumption of fuel for created car : "))
+                Cars[-1].set_consumption(cons)
 
+                # if custom_continue == 1:
+                #     input(custom_car)
 
         if to_cars == 2:
             if not Cars:
@@ -140,7 +175,7 @@ try:
 
             selected_car = int(input("Write a car number : "))
 
-            print(f"you are chose {Cars[i].get_make()}, how do you want to interact?")
+            print(f"you are chose {Cars[selected_car].get_make()}, how do you want to interact?")
 
             options = int(input(f"available function is : \n1 - to print color of car \n""2 - to print volume of tank "
                                 "\n""3 - to print of current fuel \n""4 - to print consumption of fuel \n: "))
@@ -149,14 +184,13 @@ try:
                 print(f"color of selected car is {Cars[i].get_сolor()}\n")
 
             if options == 2:
-                print(f"tank volume of selected car is {Cars[i].get_gas_tank()}\n")
+                print(f"tank volume of selected car is {Cars[i].get_gas_tank()} gallons\n")
 
             if options == 3:
-                print(f"current fuel of selected car is {Cars[i].get_fuel()}\n")
+                print(f"current fuel of selected car is {Cars[i].get_fuel()} gallons\n")
 
             if options == 4:
                 print(f"consumption of fuel of selected car is {Cars[i].get_consumption()}\n")
-
 
         # else:
         #     break - на будущее, для выхода из цикла
@@ -166,4 +200,3 @@ except ValueError:
     print("screw you guys, i'm going home")
 
 # Prius = Car("Toyota", "Prius","gray", 50,0,1)
-
